1a48e32e2d0f7037840e005babe0054c1446c5fc
Vildan Safin <safin@it-projects.info>
2020-07-28 16:48:52 +0500

    :ambulance: Rewrited all shity code
    Signed-off-by: Vildan Safin <safin@it-projects.info>

--- barbershop/static/src/js/barbershop.js
+++ barbershop/static/src/js/barbershop.js
@@ -4,14 +4,69 @@
     'use_strict';
 
     $(document).ready(function() {
-        $('.back-gif').hover(
+        hover_effects($('.back-gif'));
+        hover_effects($('.telegram'));
+        hover_effects($('.instagram'));
+        hover_effects($('.vk'));
+        hover_effects($('.contact'));
+        setTimeout(load_elements, 1000);
+
+        setTimeout(load_element, 2000, $('.instagram'));
+        setTimeout(load_element, 2300, $('.telegram'));
+        setTimeout(load_element, 2600, $('.vk'));
+        setTimeout(load_element, 2900, $('.contact'));
+        show_contacts();
+    });
+
+    function hover_effects(elem){
+        elem.hover(
             function() {
-                $(this).addClass("opacity-1");
-                $(this).removeClass( "opacity-05" );
+                if($(this)[0] === $('.back-gif')[0]){
+                    $(this).addClass("opacity-1");
+                    $(this).removeClass("opacity-05");
+                }
+                $(this).addClass("up-4");
               }, function() {
-                $(this).removeClass( "opacity-1" );
-                $(this).addClass("opacity-05");
+                if($(this)[0] === $('.back-gif')[0]){
+                    $(this).removeClass( "opacity-1" );
+                    $(this).addClass("opacity-05");
+                }
+                $(this).removeClass("up-4");
               }
         );
-    });
+    }
+
+    function load_element(el){
+        $(el).addClass('tansit-1');
+        if($(el)[0] === $('.back-gif')[0]){
+            $(el).addClass("opacity-05");
+        }
+        else{
+            $(el).addClass('opacity-1');
+        }
+        setTimeout(remove_transit, 1000, $(el));
+    }
+
+    function load_elements(){
+        $.each($('.to-load'), function (key, elem) {
+            $(this).addClass('tansit-1');
+            if($(this)[0] === $('.back-gif')[0]){
+                $(this).addClass("opacity-05");
+            }
+            else{
+                $(this).addClass('opacity-1');
+            }
+            setTimeout(remove_transit, 1000, $(this));
+        });
+    }
+
+    function remove_transit(el){
+        $(el).removeClass('tansit-1')
+    }
+
+    function show_contacts(){
+        $('.contact').click(function(){
+
+        });
+    }
 });
